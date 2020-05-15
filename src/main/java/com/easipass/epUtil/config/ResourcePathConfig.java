22d37c4369bf9df41b59d3d7204cef8982a0900b
ZJ <1476253236@qq.com>
2020-05-15 13:42:27 +0800

    v4.0.0 配置更新

--- src/main/java/com/easipass/epUtil/config/ResourcePathConfig.java
+++ src/main/java/com/easipass/epUtil/config/ResourcePathConfig.java
@@ -5,41 +5,41 @@ public class ResourcePathConfig {
     /**
      * 配置文件路径
      * */
-    public final static String CONFIG_PATH = "/config";
+    public final static String CONFIG_PATH = "/resources/config";
 
     /**
      * windows版谷歌驱动路径
      * */
-    public final static String WINDOWS_CHROME_DRIVER_PATH = "/chromeDriver/windows/chromedriver.exe";
+    public final static String WINDOWS_CHROME_DRIVER_PATH = "/resources/chromeDriver/windows/chromedriver.exe";
 
     /**
      * linux版谷歌驱动路径
      * */
-    public final static String LINUX_CHROME_DRIVER_PATH = "/chromeDriver/linux/chromedriver";
+    public final static String LINUX_CHROME_DRIVER_PATH = "/resources/chromeDriver/linux/chromedriver";
 
     /**
      * 报关单通讯回执路径
      * */
-    public final static String TONG_XUN_FORM_RESULT_PATH = "/formResult/tongXunFormResult";
+    public final static String TONG_XUN_FORM_RESULT_PATH = "/resources/formResult/tongXunFormResult";
 
     /**
      * 报关单业务回执路径
      * */
-    public final static String YE_WU_FORM_RESULT_PATH = "/formResult/yeWuFormResult";
+    public final static String YE_WU_FORM_RESULT_PATH = "/resources/formResult/yeWuFormResult";
 
     /**
      * 修撤单QP回执路径
      * */
-    public final static String QP_DEC_MOD_RESULT_PATH = "/decModResult/QPDecModResult";
+    public final static String QP_DEC_MOD_RESULT_PATH = "/resources/decModResult/QPDecModResult";
 
     /**
      * 报关单业务回执路径
      * */
-    public final static String YE_WU_DEC_MOD_RESULT_PATH = "/decModResult/yeWuDecModResult";
+    public final static String YE_WU_DEC_MOD_RESULT_PATH = "/resources/decModResult/yeWuDecModResult";
 
     /**
      * 代理委托回执路径
      * */
-    public final static String AGENT_RESULT_PATH = "/agentResult.wts";
+    public final static String AGENT_RESULT_PATH = "/resources/agentResult.wts";
 
 }
