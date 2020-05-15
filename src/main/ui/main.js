7c209aa2612a01bd5b9d0d26c2ec7140b30bd21e
ZJ0724 <June3259>
2020-05-16 00:59:13 +0800

    提交

--- src/main/ui/main.js
+++ src/main/ui/main.js
@@ -1,3 +1,4 @@
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/base.css\" />");
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"/frame/layui/css/layui.css\" />");
 document.write("<script src=\"/frame/layui/layui.all.js\"></script>");
+document.write("<script src=\"/frame/jquery-3.5.1.min.js\"></script>");
