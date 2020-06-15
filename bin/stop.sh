8d4f35fa7957d4e2a2f02cdc5cc793f8462d1e22
ZJ <1476253236@qq.com>
2020-06-15 10:16:50 +0800

    优化日志 解决乱码问题

--- bin/stop.sh
+++ bin/stop.sh
@@ -4,7 +4,7 @@
 
 # shellcheck disable=SC2009
 
-pid=$(ps -ef | grep "epUtil" | grep -v grep | awk '{print $2}')
+pid=$(ps -ef | grep "java -jar ../lib/epUtil.jar --spring.config.location=../config/application.properties" | grep -v grep | awk '{print $2}')
 
 if [ "${pid}" = "" ]; then
 
