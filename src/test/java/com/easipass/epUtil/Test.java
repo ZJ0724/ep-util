6985c2aec2521b09b2c77d98af47d552d0b8ba69
ZJ0724 <June3259>
2020-06-23 21:49:57 +0800

    测试类更新

--- src/test/java/com/easipass/epUtil/Test.java
+++ src/test/java/com/easipass/epUtil/Test.java
@@ -1,16 +1,3 @@
 package com.easipass.epUtil;
 
-import com.easipass.epUtil.component.Http;
-import com.easipass.epUtil.module.DaKaModule;
-
-public class Test {
-
-    public static void main(String[] args) throws Exception {
-
-        Http http = new Http("http://localhost:8002/config/get", "GET", null);
-
-        System.out.println(http.send());
-
-    }
-
-}
+public class Test {}
