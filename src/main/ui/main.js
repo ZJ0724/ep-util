3540241803341287cf66e86b9bb2462709ddb2ce
ZJ0724 <June3259>
2020-07-19 11:53:41 +0800

    commit

--- src/main/ui/main.js
+++ src/main/ui/main.js
@@ -1,13 +1,2 @@
-import baseRouter from "./common/routerCommon.js";
-
-(function () {
-
-    // 设置路由监听
-    window.onhashchange = function () {
-        baseRouter.load();
-    }
-
-    // 页面初始化判断路由
-    baseRouter.load();
-
-})();
+// 基础样式
+document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"/style/base.css\" />");
--- src/main/ui/page/cusFileComparison/api/websocket/CusFileComparisonWebsocketApi.js
+++
@@ -1,9 +0,0 @@
-(function (websocket) {
-    websocket.CusFileComparisonWebsocketApi = class CusFileComparisonWebsocketApi extends websocket.BaseWebsocketApi {
-
-        constructor(id) {
-            super("cusFileComparison/" + id);
-        }
-
-    };
-})(window.epUtil.api.websocket);
