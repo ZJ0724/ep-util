83156d6aed91ab180d9c3720e3e30b6892a06d1a
ZJ <1476253236@qq.com>
2020-07-21 14:57:02 +0800

    commit

---
+++ src/main/ui/api/service/daKaServiceApi.js
@@ -0,0 +1,57 @@
+import baseServiceApi from "./baseServiceApi.js";
+
+let daKaServiceApi = {
+
+    daKa: "daKa/",
+
+    // 开启自动打卡
+    openAutoDaKa() {
+        return baseServiceApi.sendHttp({
+            type: "POST",
+            url: `${daKaServiceApi.daKa}openAutoDaKa`
+        });
+    },
+
+    // 关闭打卡
+    closeAutoDaKa() {
+        return baseServiceApi.sendHttp({
+            type: "POST",
+            url: `${daKaServiceApi.daKa}closeAutoDaKa`
+        });
+    },
+
+    // 获取自动打卡状态
+    getStatus() {
+        return baseServiceApi.sendHttp({
+            type: "GET",
+            url: `${daKaServiceApi.daKa}getStatus`
+        });
+    },
+
+    // 获取打卡日志
+    getLog() {
+        return baseServiceApi.sendHttp({
+            type: "GET",
+            url: `${daKaServiceApi.daKa}getLog`
+        });
+    },
+
+    // 清空日志
+    cleanLog() {
+        return baseServiceApi.sendHttp({
+            type: "POST",
+            url: `${daKaServiceApi.daKa}cleanLog`
+        });
+    },
+
+    // 手动打卡
+    manualKaKa() {
+        return baseServiceApi.sendHttp({
+            type: "POST",
+            url: `${daKaServiceApi.daKa}manualKaKa`
+        });
+    }
+
+};
+
+export default daKaServiceApi;
