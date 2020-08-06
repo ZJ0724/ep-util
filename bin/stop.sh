9c3968a92dfbee5d8b3c6e22fc17357a8f3af876
ZJ <1476253236@qq.com>
2020-08-06 12:23:21 +0800

    新增重启脚本

--- bin/stop.sh
+++ bin/stop.sh
@@ -3,7 +3,7 @@
 # 项目根目录
 ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
 # 启动命令
-command=java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}
+command="java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}"
 # 进程id
 pid=$(ps -aux | grep ${command} | grep -v "grep" | awk '{print $2}')
 
