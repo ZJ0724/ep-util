47b5e25fb9222654b602dc6ed5c04e527422943f
ZJ <1476253236@qq.com>
2020-08-06 12:20:47 +0800

    新增重启脚本

--- bin/run.sh
+++ bin/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# epUtil进程id
-epUtilPid=$(ps -aux | grep "java -jar ../lib/ep-util.jar --spring.config.location=../config/application.properties" | grep -v "grep" | awk '{print $2}')
+# 项目根目录
+ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
+# 启动命令
+command=java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}
+# 进程id
+pid=$(ps -aux | grep ${command} | grep -v "grep" | awk '{print $2}')
 
 # 检查是否已经运行
 if [ "${epUtilPid}" != "" ]; then
-    echo "run fail,ep-util is run!"
+    echo "run fail, ep-util is run!"
     exit 0
 fi
 
-# 项目根目录
-ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
-
-nohup java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH} > /dev/null 2>&1 &
+# 启动
+nohup ${command} > /dev/null 2>&1 &
 
 echo "ep-util is run!"
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# shellcheck disable=SC2006
+# 项目根目录
+ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
+# 启动命令
+command=java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}
+# 进程id
+pid=$(ps -aux | grep ${command} | grep -v "grep" | awk '{print $2}')
 
-# shellcheck disable=SC2009
-
-pid=$(ps -ef | grep "java -jar ../lib/ep-util.jar --spring.config.location=../config/application.properties" | grep -v grep | awk '{print $2}')
-
+# 如果没运行，不进行关闭
 if [ "${pid}" = "" ]; then
 
   echo "ep-util is not run!"
