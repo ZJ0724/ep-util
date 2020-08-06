02ea0be9ca4f46ac56f71760e87d72fd14cab0f3
ZJ0724 <June3259>
2020-08-06 14:05:55 +0800

    update bin

--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# 项目根目录
-ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
-# 启动命令
-command="java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}"
-# 进程id
-pid=$(ps -aux | grep ${command} | grep -v "grep" | awk '{print $2}')
+# base.sh
+. ./base.sh
 
 # 检查是否已经运行
-if [ "${epUtilPid}" != "" ]; then
-    echo "run fail, ep-util is run!"
+if [ "${pid}" != "" ]; then
+    echo -e "\033[0;31;1m run fail, ep-util is run! \033[0m"
     exit 0
 fi
 
 # 启动
-nohup ${command} > /dev/null 2>&1 &
+nohup java -jar ${projectPath}/lib/ep-util.jar --spring.config.location=${projectPath}/config/application.properties ${projectPath} > /dev/null 2>&1 &
 
-echo "ep-util is run!"
+echo -e "\033[0;32;1m ep-util is run! \033[0m"
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-# 项目根目录
-ABSOLUTE_PATH=$(cd `dirname $0`;pwd)/../
-# 启动命令
-command="java -jar ${ABSOLUTE_PATH}lib/ep-util.jar --spring.config.location=${ABSOLUTE_PATH}config/application.properties ${ABSOLUTE_PATH}"
-# 进程id
-pid=$(ps -aux | grep ${command} | grep -v "grep" | awk '{print $2}')
+# base.sh
+. ./base.sh
 
-# 如果没运行，不进行关闭
-if [ "${pid}" = "" ]; then
-
-  echo "ep-util is not run!"
-
-else
-
+if [ "${pid}" != "" ]; then
   kill -9 "${pid}"
-
-  echo "ep-util is stop!"
-
+  echo -e "\033[0;32;1m ep-util is stop! \033[0m"
 fi
