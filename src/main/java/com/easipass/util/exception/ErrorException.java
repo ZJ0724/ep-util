3cf18f03d350b40f00b8a01c53d21617d4e375c7
ZJ0724 <June3259>
2020-08-11 21:06:32 +0800

    commit

--- src/main/java/com/easipass/util/exception/ErrorException.java
+++ src/main/java/com/easipass/util/exception/ErrorException.java
@@ -1,5 +1,8 @@
 package com.easipass.util.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 错误异常
  *
@@ -8,12 +11,18 @@ package com.easipass.util.exception;
 public final class ErrorException extends RuntimeException {
 
     /**
+     * 日志
+     * */
+    private static final Logger log = LoggerFactory.getLogger(ErrorException.class);
+
+    /**
      * 构造函数
      *
      * @param message 错误信息
      * */
     public ErrorException(String message) {
         super(message);
+        log.error(message, this);
     }
 
 }
