11f5a65e4cea45a1635620495a1c15ba8c412fd1
ZJ <1476253236@qq.com>
2021-03-26 09:54:12 +0800

    commit

--- src/test/java/com/easipass/util/test/Main.java
+++ src/test/java/com/easipass/util/test/Main.java
@@ -5,6 +5,7 @@ import com.easipass.util.service.ConfigService;
 import com.easipass.util.service.CusResultService;
 import com.easipass.util.service.impl.ConfigServiceImpl;
 import com.easipass.util.service.impl.CusResultServiceImpl;
+import com.zj0724.common.component.Sftp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,9 @@ public final class Main {
 
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Sftp sftp = new Sftp("192.168.131.211", 2333, "cusclient_test", "easipass");
+        System.out.println(123);
+    }
 
 }
